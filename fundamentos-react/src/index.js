5e65048c37bf65700c95e4a44ec5ad7d34f49ab5
giceliojr <138838427+giceliojr@users.noreply.github.com>
2023-07-13 23:48:44 -0300

    criação de componentes

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Primeiro from './componentes/basicos/Primeiro';
+import ComParametro from './componentes/basicos/ComParamentro';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+ReactDOM.render(
+    <div>
+        <Primeiro></Primeiro>
+        <ComParametro titulo="Situação do aluno(a)" aluno="Gicélio" nota={9.3} />
+        <ComParametro titulo="Situação do aluno(a)" aluno="Nataline" nota={9.9}/>
+    </div>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
